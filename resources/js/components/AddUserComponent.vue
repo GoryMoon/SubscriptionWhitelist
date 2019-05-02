6bcc213961bc2492b080824aacc64aa3481529ed
GoryMoon <gurreja@gmail.com>
2019-05-03 01:22:55 +0200

    Made tabbing while adding names change row instead of selecting button

--- resources/js/components/AddUserComponent.vue
+++ resources/js/components/AddUserComponent.vue
@@ -6,13 +6,14 @@
         </div>
         <div class="form-row" v-for="(name, index) in nameList">
             <div class="form-group col-10 col-md-8">
-                <input type="text" class="form-control" name="usernames[]" placeholder="Username" v-bind:value="name.name" v-on:input="name.name = update(index, $event.target.value)">
+                <label :for="'add-name-' + index" class="sr-only">Username</label>
+                <input type="text" class="form-control" name="usernames[]" :id="'add-name-' + index" placeholder="Username" v-bind:value="name.name" v-on:input="name.name = update(index, $event.target.value)">
             </div>
             <div v-if="index" class="form-group col-1">
-                <button class="btn btn-danger" @click.prevent v-on:click="nameList.splice(index, 1)"><fa icon="times"></fa></button>
+                <button class="btn btn-danger" tabindex="-1" @click.prevent v-on:click="nameList.splice(index, 1)"><fa icon="times"></fa></button>
             </div>
             <div v-else class="form-group col-1">
-                <button class="btn btn-success" @click.prevent v-on:click="addInput"><fa icon="plus"></fa></button>
+                <button class="btn btn-success" tabindex="-1" @click.prevent v-on:click="addInput"><fa icon="plus"></fa></button>
             </div>
         </div>
         <button type="submit" class="btn btn-primary"><fa icon="plus"></fa> Add</button>
