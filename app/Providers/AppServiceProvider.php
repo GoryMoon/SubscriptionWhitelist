e1c452b763498b3fc5a59c1d4b67a953f3b038be
GoryMoon <gurreja@gmail.com>
2021-04-11 20:30:34 +0200

    Fixed pagination not using bootstrap

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\ServiceProvider;
@@ -27,6 +28,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Paginator::useBootstrap();
         Redis::enableEvents();
         Blade::aliasComponent('components.bold', 'bold');
     }
