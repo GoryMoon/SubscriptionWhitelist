f75e635886b0db391f1b5e730717fd7643764cd9
GoryMoon <gurreja@gmail.com>
2021-10-21 21:29:21 +0200

    Added former patron and total pledged filters to Patreon links

--- resources/js/components/PatreonLinksComponent.vue
+++ resources/js/components/PatreonLinksComponent.vue
@@ -8,11 +8,16 @@
                 <b-tab title="Pledge amount">
                     <b-form-group>
                         <label for="patreon_cent_min">The minimum pledged amount in cents. (Inclusive)<br/>(0 is default) (Based on the currency used on your Patreon)</label>
-                        <b-form-input v-model="centMin" type="number" class="mb-2 mr-sm-2" id="patreon_cent_min" placeholder="100"></b-form-input>
+                        <b-form-input v-model="centMin" type="number" class="mb-2 mr-sm-2" id="patreon_cent_min" placeholder="0"></b-form-input>
                     </b-form-group>
                     <b-form-group>
                         <label for="patreon_cent_max">The maximum pledged amount in cents. (Inclusive)<br/>(No upper limit as default) (Based on the currency used on your Patreon)</label>
-                        <b-form-input v-model="centMax" type="number" class="mb-2 mr-sm-2" id="patreon_cent_max" placeholder="1000"></b-form-input>
+                        <b-form-input v-model="centMax" type="number" class="mb-2 mr-sm-2" id="patreon_cent_max" placeholder="Infinite"></b-form-input>
+                    </b-form-group>
+                    <b-form-group>
+                        <b-form-checkbox v-model="total" class="ml-2" id="patreon_total" :value="true" :disabled="!hasMinOrMax" name="patreon_total" >
+                            If the cent amount should be lifetime total a user have pledged. <span v-if="!hasMinOrMax">(Requires either max or min amount)</span>
+                        </b-form-checkbox>
                     </b-form-group>
                 </b-tab>
                 <b-tab title="Tier">
@@ -20,7 +25,7 @@
                     <b-form-group>
                         <b-form-select v-model="tierSelected" :options="options">
                             <template #first>
-                                <b-form-select-option :value="null" disabled>Please select a tier</b-form-select-option>
+                                <b-form-select-option :value="null">Please select a tier (No tier)</b-form-select-option>
                             </template>
                         </b-form-select>
                     </b-form-group>
@@ -29,7 +34,12 @@
             <hr/>
             <b-form-group>
                 <b-form-checkbox v-model="payed" class="ml-2" id="patreon_payed_once" :value="true" name="patreon_payed_once" >
-                    Require the user to have payed the pledge at least once (useful if not paying upfront)
+                    Require the user to have payed the pledge at least once. (Useful if not paying upfront)
+                </b-form-checkbox>
+            </b-form-group>
+            <b-form-group>
+                <b-form-checkbox v-model="former" class="ml-2" id="patreon_former" :disabled="haveTier" :value="true" name="patreon_former" >
+                    Include former users in the list. <span v-if="haveTier">(Does not work with tiers)</span>
                 </b-form-checkbox>
             </b-form-group>
         </b-card>
@@ -90,11 +100,19 @@ name3</textarea>
                 centMin: null,
                 centMax: null,
                 payed: false,
+                total: false,
+                former: false,
             }
         },
         computed: {
+            hasMinOrMax() {
+                return this.centMax > 0 || this.centMin > 0;
+            },
             useTiers() {
                 return this.selectedTab === 1;
+            },
+            haveTier() {
+              return this.tierSelected != null;
             },
             options() {
                 let opt = [];
@@ -113,13 +131,19 @@ name3</textarea>
                     {
                         query += (query.length > 0 ? '&' : '') + 'max=' + this.centMax;
                     }
+                    if (this.total && (this.centMax > 0 || this.centMin > 0)) {
+                        query += (query.length > 0 ? '&': '') + 'to=1'
+                    }
                 } else {
-                    if (this.tierSelected != null) {
+                    if (this.haveTier) {
                         query += 't=' + this.tierSelected;
                     }
                 }
                 if (this.payed) {
                     query += (query.length > 0 ? '&': '') + 'py=1'
+                }
+                if (this.former && !this.haveTier) {
+                    query += (query.length > 0 ? '&': '') + 'f=1'
                 }
                 return query.length > 0 ? '?' + query: '';
             }
